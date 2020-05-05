bc6c8d286f0e6f1a55e27bb35b6f1d8861238aa5
Marko Pahić <marco.pahic@swisscom.com>
2020-05-05 10:08:27 +0200

    init commit

---
+++ custom_components/google_firebase_store/__init__.py
@@ -0,0 +1,89 @@
+"""Support for Google Cloud Firebase"""
+import datetime
+import json
+import logging
+import os
+from typing import Any, Dict
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+import voluptuous as vol
+
+from homeassistant.const import EVENT_STATE_CHANGED, STATE_UNAVAILABLE, STATE_UNKNOWN
+from homeassistant.core import Event, HomeAssistant
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entityfilter import FILTER_SCHEMA
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "google_firebase_store"
+
+CONF_SERVICE_PRINCIPAL = "credentials_json"
+CONF_FILTER = "filter"
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_SERVICE_PRINCIPAL): cv.string,
+                vol.Optional(CONF_FILTER): FILTER_SCHEMA,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
+    """Activate Google Firebase component."""
+
+    config = yaml_config[DOMAIN]
+    service_principal_path = os.path.join(
+        hass.config.config_dir, config[CONF_SERVICE_PRINCIPAL]
+    )
+
+    if not os.path.isfile(service_principal_path):
+        _LOGGER.error("Path to credentials file cannot be found")
+        return False
+
+    entities_filter = config[CONF_FILTER]
+
+    cred = credentials.Certificate(service_principal_path)
+    default_app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+
+
+    def send_to_pubsub(event: Event):
+        """Send states to Firebase """
+        state = event.data.get("new_state")
+        if (
+            state is None
+            or state.state in (STATE_UNKNOWN, "", STATE_UNAVAILABLE)
+            or not entities_filter(state.entity_id)
+        ):
+            return
+
+        doc_ref = db.collection(u'homeassistant').document(state.entity_id)
+
+        doc_ref.set(state.as_dict())
+
+    hass.bus.listen(EVENT_STATE_CHANGED, send_to_pubsub)
+    
+    def fire_event(col_snapshot, changes, read_time):
+        print(u'Callback received query snapshot.')
+        print(u'Current triggers:')
+        for change in changes:
+            if change.type.name == 'MODIFIED':
+                hass.callService('homeassistant', 'turn_on', {
+                  entity_id: 'input_number.'+ u'{}'.format(doc.id)
+                });
+                _LOGGER.warning("Firebase plugin fire: " + u'{}'.format(change.document.id))
+
+    col_query = db.collection(u'triggers')
+
+    # Watch the collection query
+    query_watch = col_query.on_snapshot(fire_event)
+
+    return True
+
